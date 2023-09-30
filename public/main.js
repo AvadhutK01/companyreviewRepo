493362e177f02f1c953faded5ab622caa9018ce8
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-09-30 22:23:37 +0530

    optimized some code and added gitignore

--- public/main.js
+++ public/main.js
@@ -1,47 +1,55 @@
-const fetchData = async () => {
-    try {
-        const div = document.getElementById('data-div');
-        const companyName = document.getElementById('search').value;
-        const response = await axios.get(`/post-search/${companyName}`);
+const div = document.getElementById('data-div');
+const searchForm = document.getElementById('search-form');
 
-        if (response.data.length > 0) {
-            const latestData = response.data[response.data.length - 1];
-            const companyName = latestData.companyname;
-            const overallRating = latestData.overallrating;
-            const resultDiv = document.createElement('div');
-            div.textContent = "";
-            resultDiv.className = "mt-3 alert alert-primary";
-            resultDiv.role = "alert";
-            resultDiv.innerHTML = `<h4>Company Name: ${companyName}</h4><p><h5> Overall Rating: ${overallRating}/5</h5></p>`;
-            div.appendChild(resultDiv);
+const displayData = (data) => {
+    div.textContent = '';
 
-            for (let i = 0; i < response.data.length; i++) {
-                const data = response.data[i];
-                const cardContainer = document.createElement('div');
-                cardContainer.className = "card-container";
+    if (data.length > 0) {
+        const latestData = data[data.length - 1];
+        const { companyname, overallrating } = latestData;
 
-                cardContainer.innerHTML = `
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Company Name: ${data.companyname}</h5>
-                            <p class="card-text">Pros: ${data.pros}</p>
-                            <p class="card-text">Cons: ${data.cons}</p>
-                            <p class="card-text">Rating: ${data.rating}/5</p>
-                        </div>
+        const resultDiv = document.createElement('div');
+        resultDiv.className = 'mt-3 alert alert-primary';
+        resultDiv.role = 'alert';
+        resultDiv.innerHTML = `<h4>Company Name: ${companyname}</h4><p><h5> Overall Rating: ${overallrating}/5</h5></p>`;
+        div.appendChild(resultDiv);
+
+        data.forEach((item) => {
+            const { companyname, pros, cons, rating } = item;
+
+            const cardContainer = document.createElement('div');
+            cardContainer.className = 'card-container';
+
+            cardContainer.innerHTML = `
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Company Name: ${companyname}</h5>
+                        <p class="card-text">Pros: ${pros}</p>
+                        <p class="card-text">Cons: ${cons}</p>
+                        <p class="card-text">Rating: ${rating}/5</p>
                     </div>
-                `;
+                </div>
+            `;
 
-                div.appendChild(cardContainer);
-            }
-        } else {
-            div.textContent = "No data found";
-        }
-    } catch (error) {
-        console.error(error);
+            div.appendChild(cardContainer);
+        });
+    } else {
+        div.textContent = 'No data found';
     }
 };
 
-document.getElementById('search-form').addEventListener('submit', function (e) {
+const fetchData = async () => {
+    try {
+        const companyName = document.getElementById('search').value;
+        const response = await axios.get(`/post-search/${companyName}`);
+        displayData(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        div.textContent = 'Error occurred while fetching data';
+    }
+};
+
+searchForm.addEventListener('submit', function (e) {
     e.preventDefault();
     fetchData();
 });
