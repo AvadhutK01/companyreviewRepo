4c5502d3041616668cebcf7b5d462a35af022c55
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-09-30 13:17:33 +0530

    Optimized code with async await

--- controllers/datacontroller.js
+++ controllers/datacontroller.js
@@ -5,49 +5,56 @@ exports.getIndex = (req, res, next) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 }
 
-exports.postdata = (req, res, next) => {
-    const companyname = req.body.companyNamevalue;
-    const pros = req.body.prosvalue;
-    const cons = req.body.cons;
-    const rating = parseInt(req.body.rating);
-    Data.findAll({
-        where: {
-            companyname: companyname
-        }
-    }).then(results => {
+exports.postdata = async (req, res, next) => {
+    try {
+        const companyname = req.body.companyNamevalue;
+        const pros = req.body.prosvalue;
+        const cons = req.body.cons;
+        const rating = parseInt(req.body.rating);
+
+        const results = await Data.findAll({
+            where: {
+                companyname: companyname
+            }
+        });
+
         let totalRating = 0;
         let numberOfRatings = 0;
+
         results.forEach(result => {
             totalRating += result.rating;
             numberOfRatings++;
         });
+
         totalRating += rating;
         numberOfRatings++;
-        const averageRating = totalRating / numberOfRatings;
-        Data.create({
+
+        const averageRating = (totalRating / numberOfRatings).toFixed(1);
+
+        await Data.create({
             companyname: companyname,
             pros: pros,
             cons: cons,
             rating: rating,
             overallrating: averageRating
-        }).then(result => {
-            res.redirect('/');
-        }).catch(err => {
-            console.log(err);
         });
-    }).catch(err => {
+
+        res.redirect('/');
+    } catch (err) {
         console.log(err);
-    });
+    }
 };
-exports.getData = (req, res, next) => {
-    const companyname = req.params.companyname;
-    Data.findAll({
-        where: {
-            companyname: companyname
-        }
-    }).then(results => {
+
+exports.getData = async (req, res, next) => {
+    try {
+        const companyname = req.params.companyname;
+        const results = await Data.findAll({
+            where: {
+                companyname: companyname
+            }
+        });
         res.json(results);
-    }).catch(err => {
+    } catch (err) {
         console.log(err);
-    });
+    }
 };
--- public/main.js
+++ public/main.js
@@ -1,43 +1,47 @@
-let div = document.getElementById('data-div');
-document.getElementById('search-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const companyname = document.getElementById('search').value;
-    axios.get(`/post-search/${companyname}`)
-        .then((response) => {
-            if (response.data.length > 0) {
-                const latestData = response.data[response.data.length - 1];
-                const companyName = latestData.companyname;
-                const overallRating = latestData.overallrating;
+const fetchData = async () => {
+    try {
+        const div = document.getElementById('data-div');
+        const companyName = document.getElementById('search').value;
+        const response = await axios.get(`/post-search/${companyName}`);
 
-                const resultDiv = document.createElement('div');
-                resultDiv.className = "mt-3 alert alert-primary";
-                resultDiv.role = "alert";
-                resultDiv.textContent = `Company Name: ${companyName}, Overall Rating: ${overallRating}`;
-                div.appendChild(resultDiv);
+        if (response.data.length > 0) {
+            const latestData = response.data[response.data.length - 1];
+            const companyName = latestData.companyname;
+            const overallRating = latestData.overallrating;
+            const resultDiv = document.createElement('div');
+            div.textContent = "";
+            resultDiv.className = "mt-3 alert alert-primary";
+            resultDiv.role = "alert";
+            resultDiv.innerHTML = `<h4>Company Name: ${companyName}</h4><p><h5> Overall Rating: ${overallRating}/5</h5></p>`;
+            div.appendChild(resultDiv);
 
-                for (let i = 0; i < response.data.length; i++) {
-                    const data = response.data[i];
-                    const cardContainer = document.createElement('div');
-                    cardContainer.className = "card-container";
+            for (let i = 0; i < response.data.length; i++) {
+                const data = response.data[i];
+                const cardContainer = document.createElement('div');
+                cardContainer.className = "card-container";
 
-                    cardContainer.innerHTML = `
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Company Name: ${data.companyname}</h5>
-                                <p class="card-text">Pros: ${data.pros}</p>
-                                <p class="card-text">Cons: ${data.cons}</p>
-                                <p class="card-text">Rating: ${data.rating}</p>
-                            </div>
+                cardContainer.innerHTML = `
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Company Name: ${data.companyname}</h5>
+                            <p class="card-text">Pros: ${data.pros}</p>
+                            <p class="card-text">Cons: ${data.cons}</p>
+                            <p class="card-text">Rating: ${data.rating}/5</p>
                         </div>
-                    `;
+                    </div>
+                `;
 
-                    div.appendChild(cardContainer);
-                }
-            } else {
-                div.textContent = "No data found";
+                div.appendChild(cardContainer);
             }
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
+        } else {
+            div.textContent = "No data found";
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    fetchData();
 });
